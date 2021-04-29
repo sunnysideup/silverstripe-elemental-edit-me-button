e7f4b3fd72662834b9f656f4ebce0b9857f474ed
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-04-30 10:05:43 +1200

    PATCH: adding check for ? in link

--- src/Form/GridFieldDetailFormItemRequestExtension.php
+++ src/Form/GridFieldDetailFormItemRequestExtension.php
@@ -21,6 +21,9 @@ class GridFieldDetailFormItemRequestExtension extends Extension
                     </div>'
                 )
             );
+            if (! strpos($link, '?')) {
+                $link .= '?';
+            }
             $link = str_replace('?', '?stage=Stage&', $link);
             $actions->push(
                 LiteralField::create(
