0d9d97086dc769d28a88ebf1c2b8b971df43a8e0
Nicolaas <github@sunnysideup.co.nz>
2023-07-12 15:14:38 +1200

    WIP

--- src/Extensions/AddCanEditButton.php
+++ src/Extensions/AddCanEditButton.php
@@ -3,34 +3,39 @@
 namespace Sunnysideup\ElementalEditMeButton\Extensions;
 
 use DNADesign\Elemental\Extensions\ElementalPageExtension;
+use SilverStripe\CMS\Controllers\ContentController;
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\Convert;
 use SilverStripe\Core\Extension;
 use SilverStripe\View\Requirements;
+use Page;
 
 /**
  * Class \Sunnysideup\ElementalEditMeButton\Extensions\AddCanEditButton
  *
- * @property PageController|AddCanEditButton $owner
+ * @property PageController $owner
  */
 class AddCanEditButton extends Extension
 {
     public function onAfterInit()
     {
-        if ($this->owner->dataRecord->hasExtension(ElementalPageExtension::class)) {
-            $elementArea = $this->owner->ElementalArea();
-            $elements = $elementArea->Elements();
-            $element = $elements->first();
-            $elementIds = [];
-            //we assume that if you can edit one, you can edit all ...
-            if ($element && $element->canEdit()) {
-                $elementIds = $elements->columnUnique();
-                if (count($elementIds) > 0) {
-                    $js = $this->getButton();
-                    Requirements::customScript(
-                        'let ElementalEditMeButtonIds = ' . Convert::array2json($elementIds) . ';
-                        ' . $js,
-                        'ElementalEditMeButton_ids'
-                    );
+        /** @var Page $page */
+        $page = $this->owner->data();
+        if ($page && $page->hasExtension(ElementalPageExtension::class)) {
+            $elements = $page->ElementalArea()?->Elements();
+            if($elements) {
+                $element = $elements->first();
+                //we assume that if you can edit one, you can edit all ...
+                if ($element && $element->canEdit()) {
+                    $elementIds = $elements->columnUnique();
+                    if (count($elementIds) > 0) {
+                        $js = $this->getButton();
+                        Requirements::customScript(
+                            'let ElementalEditMeButtonIds = ' . json_encode($elementIds) . ';
+                            ' . $js,
+                            'ElementalEditMeButton_ids'
+                        );
+                    }
                 }
             }
         }
