ff870b30523e69a8ad4b5e48abc2cd7bc41d8022
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-01-24 08:07:26 +1300

    FIX: cleanup

--- client/AddCanEditButton.js
+++ client/AddCanEditButton.js
@@ -1,9 +1,8 @@
 window.addEventListener('load', () => {
-  ;(function () {
-    const template = `
+  const template = `
             <div class="edit-me-button">
                 <a
-                    href="/admin/pages/edit/EditForm/$ID/field/ElementalArea/item/[ID-GOES-HERE]/edit"
+                    href="/admin/pages/edit/EditForm/[PAGE-ID-GOES-HERE]/field/ElementalArea/item/[ELEMENT-ID-GOES-HERE]/edit"
                     title="Edit in CMS"
                     target="_parent"
                 >
@@ -11,95 +10,94 @@ window.addEventListener('load', () => {
                 </a>
             </div>
         `
-    const applyTemplateToElements = function (
-      ElementalEditMeButtonIds,
-      template
-    ) {
-      for (const id in ElementalEditMeButtonIds) {
-        const key = ElementalEditMeButtonIds[id]
-        let elem = document.querySelector('#' + key)
-        if (elem) {
-          elem.style.position = 'relative'
-          const templateForMe = template.replace('[ID-GOES-HERE]', id)
-          elem.innerHTML = templateForMe + elem.innerHTML
-        }
+  const applyTemplateToElements = function (
+    ElementalEditMeButtonIds,
+    template
+  ) {
+    for (const id in ElementalEditMeButtonIds) {
+      const key = ElementalEditMeButtonIds[id]
+      let elem = document.querySelector('#' + key)
+      if (elem) {
+        elem.style.position = 'relative'
+        let templateForMe = template
+          .replace('[ELEMENT-ID-GOES-HERE]', id)
+          .replace('[PAGE-ID-GOES-HERE]', ElementalEditMeButtonPageID)
+        elem.innerHTML = templateForMe + elem.innerHTML
       }
     }
+  }
 
-    // Function to separate buttons if their parent height is less than 50px
-    const handleOverlappingButtons = function () {
-      const minHeight = 80
-      // Select all buttons with the class "edit-me-button"
-      const allButtons = Array.from(
-        document.querySelectorAll('.edit-me-button')
-      )
-      // Filter buttons where the parent's height is less than 50px
-      const overlappingButtons = allButtons.filter(button => {
-        const parentDiv = button.parentElement
-        return parentDiv && parentDiv.offsetHeight < minHeight
-      })
+  // Function to separate buttons if their parent height is less than 50px
+  const handleOverlappingButtons = function () {
+    const minHeight = 80
+    // Select all buttons with the class "edit-me-button"
+    const allButtons = Array.from(document.querySelectorAll('.edit-me-button'))
+    // Filter buttons where the parent's height is less than 50px
+    const overlappingButtons = allButtons.filter(button => {
+      const parentDiv = button.parentElement
+      return parentDiv && parentDiv.offsetHeight < minHeight
+    })
 
-      // Add event listeners to filtered buttons
-      overlappingButtons.forEach(button => {
-        button.addEventListener('mouseenter', () => {
-          let count = 0
-          if (button.classList.contains('out-of-the-way')) {
+    // Add event listeners to filtered buttons
+    overlappingButtons.forEach(button => {
+      button.addEventListener('mouseenter', () => {
+        let count = 0
+        if (button.classList.contains('out-of-the-way')) {
+          return
+        }
+        allButtons.forEach((otherButton, index) => {
+          if (
+            otherButton === button ||
+            !doButtonsOverlap(button, otherButton)
+          ) {
+            // Skip the hovered button or buttons that don't overlap
+            otherButton.style.marginTop = ``
+            otherButton.classList.remove('out-of-the-way')
             return
           }
-          allButtons.forEach((otherButton, index) => {
-            if (
-              otherButton === button ||
-              !doButtonsOverlap(button, otherButton)
-            ) {
-              // Skip the hovered button or buttons that don't overlap
-              otherButton.style.marginTop = ``
-              otherButton.classList.remove('out-of-the-way')
-              return
-            }
-            count++
-            const offsetY = minHeight * count
-            otherButton.style.marginTop = `${offsetY}px` // Apply dynamic margin-top
-            otherButton.classList.add('out-of-the-way')
-          })
-        })
-
-        let resetTimeoutId
-
-        button.addEventListener('mouseleave', () => {
-          // Clear the previous timeout if it exists
-          if (resetTimeoutId) {
-            clearTimeout(resetTimeoutId)
-          }
-
-          // Set a new timeout to reset buttons after 1 second
-          resetTimeoutId = window.setTimeout(() => {
-            allButtons.forEach(otherButton => {
-              otherButton.style.marginTop = ''
-              otherButton.classList.remove('out-of-the-way')
-            })
-
-            // Clear the timeout ID after execution
-            resetTimeoutId = null
-          }, 1000)
+          count++
+          const offsetY = minHeight * count
+          otherButton.style.marginTop = `${offsetY}px` // Apply dynamic margin-top
+          otherButton.classList.add('out-of-the-way')
         })
       })
-    }
 
-    // Function to check if two buttons overlap
-    const doButtonsOverlap = function (button1, button2) {
-      const rect1 = button1.getBoundingClientRect()
-      const rect2 = button2.getBoundingClientRect()
+      let resetTimeoutId
 
-      return !(
-        rect1.right < rect2.left ||
-        rect1.left > rect2.right ||
-        rect1.bottom < rect2.top ||
-        rect1.top > rect2.bottom
-      )
-    }
+      button.addEventListener('mouseleave', () => {
+        // Clear the previous timeout if it exists
+        if (resetTimeoutId) {
+          clearTimeout(resetTimeoutId)
+        }
 
-    applyTemplateToElements(ElementalEditMeButtonIds, template)
+        // Set a new timeout to reset buttons after 1 second
+        resetTimeoutId = window.setTimeout(() => {
+          allButtons.forEach(otherButton => {
+            otherButton.style.marginTop = ''
+            otherButton.classList.remove('out-of-the-way')
+          })
 
-    handleOverlappingButtons()
-  })()
+          // Clear the timeout ID after execution
+          resetTimeoutId = null
+        }, 1000)
+      })
+    })
+  }
+
+  // Function to check if two buttons overlap
+  const doButtonsOverlap = function (button1, button2) {
+    const rect1 = button1.getBoundingClientRect()
+    const rect2 = button2.getBoundingClientRect()
+
+    return !(
+      rect1.right < rect2.left ||
+      rect1.left > rect2.right ||
+      rect1.bottom < rect2.top ||
+      rect1.top > rect2.bottom
+    )
+  }
+
+  applyTemplateToElements(ElementalEditMeButtonIds, template)
+
+  handleOverlappingButtons()
 })
--- src/Extensions/AddCanEditButton.php
+++ src/Extensions/AddCanEditButton.php
@@ -7,9 +7,11 @@ use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\Convert;
 use SilverStripe\Core\Extension;
+use SilverStripe\Security\Permission;
 use SilverStripe\View\Requirements;
 use Page;
 use PageController;
+use SilverStripe\Security\Security;
 
 /**
  * Class \Sunnysideup\ElementalEditMeButton\Extensions\AddCanEditButton
@@ -21,24 +23,29 @@ class AddCanEditButton extends Extension
     public function onAfterInit()
     {
         /** @var Page $page */
-        $page = $this->owner->data();
-        if ($page && $page->hasExtension(ElementalPageExtension::class)) {
-            $elements = $page->ElementalArea()?->Elements();
-            if ($elements && $elements->exists()) {
-                //we assume that if you can edit one, you can edit all ...
-                $elementIds = [];
-                foreach ($elements as $element) {
-                    if ($element->canEdit()) {
-                        $elementIds[] = $element->getAnchor();
+        $user = Security::getCurrentUser();
+        if ($user && Permission::check('CMS_ACCESS_CMSMain', 'any', $user)) {
+            $page = $this->owner->data();
+            if ($page && $page->hasExtension(ElementalPageExtension::class)) {
+                $elements = $page->ElementalArea()?->Elements();
+                if ($elements && $elements->exists()) {
+                    //we assume that if you can edit one, you can edit all ...
+                    $elementIds = [];
+                    foreach ($elements as $element) {
+                        if ($element->canEdit()) {
+                            $elementIds[$element->ID] = $element->getAnchor();
+                        }
                     }
-                }
-                if (!empty($elementIds)) {
-                    Requirements::customScript(
-                        'const ElementalEditMeButtonIds = ' . json_encode(array_unique($elementIds)),
-                        'ElementalEditMeButton_ids'
-                    );
-                    Requirements::javascript('sunnysideup/elemental-edit-me-button: client/AddCanEditButton.js');
-                    Requirements::css('sunnysideup/elemental-edit-me-button: client/AddCanEditButton.css');
+                    if (!empty($elementIds)) {
+                        Requirements::customScript(
+                            '
+                                const ElementalEditMeButtonIds = ' . json_encode($elementIds) . ';
+                                const ElementalEditMeButtonPageID = ' . $page->ID . ';',
+                            'ElementalEditMeButton_ids'
+                        );
+                        Requirements::javascript('sunnysideup/elemental-edit-me-button: client/AddCanEditButton.js');
+                        Requirements::css('sunnysideup/elemental-edit-me-button: client/AddCanEditButton.css');
+                    }
                 }
             }
         }
